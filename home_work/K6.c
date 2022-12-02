22fbb069ed92823aab3ae5ff064a5994922affce
farmonoff <farmonovasad03@gmail.com>
2022-12-03 02:53:48 +0500

    03.12.2022

---
+++ home_work/K6.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+int main() {
+    srand(time(0));
+
+    int n, sum;
+    printf("Ixtiyoriy son kiriting>>>"); scanf("%d", &n);
+    int arr[n][n];
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            arr[i][j] = 1 + rand() % 9;
+            printf("%d ", arr[i][j]);
+        }
+        puts("");
+    }
+    puts("");
+
+    for(int i = 0; i < n; i++){
+        if(i <= n){
+            sum = 0;
+        } 
+        for(int j = 0; j < n; j++){
+            sum += arr[i][j];
+        }
+        printf("%d - qarorda %d\n", i + 1, sum);
+    }
+
+return 0;    
+}
